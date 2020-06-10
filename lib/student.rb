f0cfd1b7f95403e598e37c70fd060cd6f7bcb462
arnie b <arnold.bwaila@gmail.com>
2020-06-10 16:22:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,15 @@
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+attr_accessor :name, :grade
+attr_reader :id 
+
+def initilize(name, grade, id=nil)
+  @name = name
+  @grade = grade
+  @id = id 
+end 
+
+def self.create_table
+  sql = CREATE TABLE IF NOT EXIST()
   
 end
